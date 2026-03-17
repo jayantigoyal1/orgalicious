3e3f132e914d65e3c840a293e4044afe8aeff0df
Jayanti Goyal <jayantigoyal1911@gmail.com>
2026-03-17 23:30:00 +0530

    corrections

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,11 +1,10 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'orgalicious',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '',
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
